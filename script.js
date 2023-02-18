60e03913a9c7de645d17475bfc1cfd61fb286c6c
shaikhanas02 <122043549+shaikhanas02@users.noreply.github.com>
2023-02-18 17:46:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-//your code here
-let maximum = 20, minimum = -20;
- function displayRandomNumber() {
- document.getElementById("num").
-innerHTML = Math.floor(Math.random() *
-(40)) - 20;
- }
+
+var input=parseInt(document.getElementById("guess").value);
+ var numP=document.getElementById("num");
+var res=document.getElementById("respond");
+function randomNumGen(){
+	 //generate random number
+var	randomNum = Math.floor(Math.random()*41) - 20;
+	 //show random number
+	numP.innerText = randomNum;
+	
+  //check differnce and print
+	if(Math.abs(randomNum - input) <= 5){
+		res.innerText = "Hot";
+	}
+	else{
+		res.innerText = "Cold";
+	}
+}
+
+
+	
